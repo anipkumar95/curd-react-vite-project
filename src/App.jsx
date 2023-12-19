ffc8b1ea0c4f8d3c5e32df0041d44e05fefe3bde
anip kumar <152984369+anipkumar95@users.noreply.github.com>
2023-12-19 14:52:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import Create from "./components/Create";
+import Show from "./components/Show";
+
+
+const App = () => {
+  const [users, setusers] = useState(
+    JSON.parse(localStorage.getItem("users")) || []
+  );
+
+  
+  return (
+
+    <div>
+      <Create users={users} setusers={setusers} />
+      <Show users={users} setusers={setusers} />
+    </div>
+
+  );
+  
+  
+};
+
+export default App;
---
+++ src/components/Show.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { toast } from "react-toastify";
+
+
+const Show = (props) => {
+    const { users, setusers } = props;
+
+    const DeleteHandler = (index) => {
+        const copyusers = [...users];
+        copyusers.splice(index, 1);
+        setusers(copyusers);
+        localStorage.setItem("users", JSON.stringify(copyusers));
+        toast.warn("user deleted successfully!")
+    };
+
+    return(
+
+        <ol className="flex justify-start flex-col mt-5 w-1/2 m-auto container">
+        <h1 className="text-2xl mb-4">Registered Users</h1>
+
+        {users.length > 0 ? users
+          .map((user, index) => {
+            return (
+              <li
+                key={index}
+                className="flex justify-between mb-4 text-1xl p-4 bg-indigo-100 rounded-xl"
+              >
+                {user.username}
+
+                <p>
+                  <span className="mr-6 cursor-progress">
+                    ✏️
+                    </span>
+                  <span className="cursor-pointer" onClick={() => DeleteHandler(index)}>
+                    ❌
+                  </span>
+                </p>
+              </li>
+            );
+          })
+          .reverse()
+          :"Loading..."
+        }
+        <hr className="mt-4" />
+      </ol>
+
+    );
+};
+
+export default Show;
